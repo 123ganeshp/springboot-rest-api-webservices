6974635fbb0427bfba504b86b047d0160805e720
Ganesh pawar <ganeshpawar@Ganeshs-MacBook-Air.local>
2025-11-26 15:56:07 +0530

    Initial commit - Basic CRUD + README added

---
+++ README.md
@@ -0,0 +1,15 @@
+# Spring Boot Product CRUD Application
+
+This project contains basic CRUD operations using Spring Boot and MySQL.
+
+## Features
+- Add Product
+- Update Product
+- Delete Product
+- Get Products
+
+More features will be added using Git branching strategy:
+- DTO Pattern
+- Exception Handling
+- Validation
+- Logging
---
+++ src/main/java/com/restapi/service/UserService.java
@@ -0,0 +1,20 @@
+package com.restapi.service;
+
+import com.restapi.entity.User;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public interface UserService {
+
+    User createUser(User user);
+
+    User getUserById(Long userId);
+
+    List<User> getAllUsers();
+
+    User updateUser(User user);
+
+    void deleteUser(Long userId);
+}
