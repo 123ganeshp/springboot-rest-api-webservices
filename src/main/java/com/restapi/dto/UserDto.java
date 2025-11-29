7e05a9a260c63d013a0ef7e76dc2aca964ef53ec
Ganesh pawar <ganeshpawar@Ganeshs-MacBook-Air.local>
2025-11-29 13:28:25 +0530

    feat(validation): add DTO validation with @Valid and custom messages

--- src/main/java/com/restapi/dto/UserDto.java
+++ src/main/java/com/restapi/dto/UserDto.java
@@ -1,5 +1,7 @@
 package com.restapi.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,7 +13,11 @@ import lombok.Setter;
 @NoArgsConstructor
 public class UserDto {
     private Long id;
+    @NotEmpty(message = "User First name should not be Null or Empty")
     private String firstName;
+    @NotEmpty(message = "User Last name should not be Null or Empty")
     private String lastName;
+    @NotEmpty(message = "User Email should not be Null or Empty")
+    @Email(message = "Email address should be valid")
     private String email;
 }
